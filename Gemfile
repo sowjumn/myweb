3e37f340d2f335577dd884b757dac7d1600e865f
sowjanya <sowju19@gmail.com>
2014-01-30 14:15:03 -0800

    First commit

---
+++ Gemfile
@@ -0,0 +1,34 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.2'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'jquery-rails'
+gem 'mustache'
+gem 'hogan_assets'
+
+group :development, :test do
+	gem 'awesome_print'
+	gem 'rspec-rails'
+end
+
+group :test do
+	gem 'selenium-webdriver'
+	gem 'capybara'
+	gem 'cucumber-rails', require: false
+	gem 'database_cleaner'
+end
+
+group :production do
+	gem 'rails_12factor'
+end
+
+
+# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myweb::Application.config.secret_key_base = '5d929c2c20f861a5fe03f1cc0359cedbc5d72c2c1eac9c97e988b8bbafbe599b470507db70aa3b5c0541848a4a6b58d9b6ba0af59d95022df8f2529f1394dc58'
