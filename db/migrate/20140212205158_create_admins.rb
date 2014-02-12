3ff5e5dbd71863f0e78d4f78f3bce848a718c36a
sowjanya <sowju19@gmail.com>
2014-02-12 12:54:01 -0800

    Added Admins table

---
+++ db/migrate/20140212205158_create_admins.rb
@@ -0,0 +1,9 @@
+class CreateAdmins < ActiveRecord::Migration
+  def change
+    create_table :admins do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+    end
+  end
+end
