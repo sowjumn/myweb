da56fc34ef0d553a626d3905bf77767de4f4700e
sowjanya <sowju19@gmail.com>
2014-03-16 22:31:22 -0700

    Added blogs functionality

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,49 @@
 class BlogsController < ApplicationController
+  before_action :signed_in_admin, only: [:new, :create]
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_url
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update_attributes(blog_params)
+      redirect_to blogs_url
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @blogs_all = Blog.all
+    @blog = Blog.last
+  end
+
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+    redirect_to blogs_url
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+
+  private
+    def blog_params
+      params.require(:blog).permit(:title, :para1, :para2, :para3)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140315051012) do
+ActiveRecord::Schema.define(version: 20140317011923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,5 +27,14 @@ ActiveRecord::Schema.define(version: 20140315051012) do
   end
 
   add_index "admins", ["remember_token"], name: "index_admins_on_remember_token", using: :btree
+
+  create_table "blogs", force: true do |t|
+    t.string   "title"
+    t.text     "para1"
+    t.text     "para2"
+    t.text     "para3"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
