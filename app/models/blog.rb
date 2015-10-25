8f62c6767ed11c63bfbd84304169f207edd92439
sowjanya <sowju19@gmail.com>
2015-10-24 18:22:43 -0700

    added image sizes and styles

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,4 @@
 class Blog < ActiveRecord::Base
-  has_attached_file :avatar, styles: { large: "1440x290>", medium: "150x150>", thumb: "50x50" }, default_url: "/images/:style/missing.png" 
+  has_attached_file :avatar, styles: { large: "2560x440#", medium: "150x150!", thumb: "50x50!" }, default_url: "/images/:style/missing.png" 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
