617c4176ddc9964e1d52245ceda4524d9c1b5ccf
sowjanya <sowju19@gmail.com>
2015-12-22 15:31:09 -0800

    adding variables to bash script

--- push.sh
+++ push.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-git push origin master
-git push heroku master
+if [ "$1" = "heroku" ]
+then 
+  git push heroku master
+elif [ "$1" = "github" ]
+then
+  git push origin master
+else
+  git push origin master
+  git push heroku master
+fi
